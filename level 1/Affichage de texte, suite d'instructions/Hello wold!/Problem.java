686df4bc6b5471c2f7fc101d823913e88d39a8f9
Bénas Thomas <benas.thomas@gmail.com>
2019-07-25 10:31:41 +0200

    Update Problem.java

--- level 1/Affichage de texte, suite d'instructions/Hello wold!/Problem.java
+++ level 1/Affichage de texte, suite d'instructions/Hello wold!/Problem.java
@@ -1,10 +1,8 @@
-package FranceIOI;
-
 /**
  *
- * @author thomas
+ * @author thomas (lamford sur France-IOI)
  */
-public class Problem {
+public class Main {
 
     /**
      * Écrivez un programme permettant au robot d'afficher précisément le texte Hello world!.
