7204f1e9b6c55bb11420b0468cedff5a38cdce04
Thomas <akgx99@gmail.com>
2019-07-25 10:43:28 +0200

    add level 1 recette

---
+++ level 1/Affichage de texte, suite d'instructions/Recette secrète/Problem.java
@@ -0,0 +1,24 @@
+import static algorea.Robot.*;
+
+/**
+ *
+ * @author thomas (lamford sur France-IOI)
+ */
+public class Main {
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        remplir(3);
+        transferer(3,5);
+        
+        remplir(3);
+        transferer(3,5);
+        
+        vider(5);
+        transferer(3,5);
+        remplir(3);
+        transferer(3,5);
+    }
+    
+}
