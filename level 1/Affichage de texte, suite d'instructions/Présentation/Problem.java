92d7756e19189ba7f56ed7d93626ba8376a39cab
Thomas <akgx99@gmail.com>
2019-07-25 10:27:08 +0200

    add level presentation

---
+++ level 1/Affichage de texte, suite d'instructions/Présentation/Problem.java
@@ -0,0 +1,16 @@
+package FranceIOI;
+/**
+ *
+ * @author thomas (lamford sur France-IOI)
+ */
+public class Problem {
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        System.out.println("Coucou !");
+        System.out.println("Je m'appelle Camthalion");
+        System.out.println("Ma devise est 'Parler peu mais parler bien'.");
+    }
+    
+}
