849154b7cee20caf5efdabcd9700c00283528f44
Thomas <akgx99@gmail.com>
2019-07-25 10:15:48 +0200

    add level 1 presentation

---
+++ level 1/Affichage de texte, suite d'instructions/Présentation/Problem.java
@@ -0,0 +1,21 @@
+package FranceIOI;
+
+/**
+ *
+ * @author thomas
+ */
+public class Problem {
+
+    /**
+     * Écrivez un programme permettant d'afficher précisément le texte : 
+     * Coucou !
+     * Je m'appelle Camthalion
+     * Ma devise est 'Parler peu mais parler bien'.
+     */
+    public static void main(String[] args) {
+        System.out.println("Coucou !");
+        System.out.println("Je m'appelle Camthalion");
+        System.out.println("Ma devise est 'Parler peu mais parler bien'.");
+    }
+    
+}
