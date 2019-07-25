ffeba27837175a08f46dcfca07a4f750dbe0720e
Thomas <akgx99@gmail.com>
2019-07-25 10:23:41 +0200

    add level 1 fourré & montage

---
+++ level 1/Affichage de texte, suite d'instructions/Dans le fourré/Problem.java
@@ -0,0 +1,22 @@
+package FranceIOI;
+import static algorea.Robot.*;
+/**
+ *
+ * @author thomas (lamford sur France-IOI)
+ */
+public class Problem {
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        haut();
+        haut();
+        haut();
+        droite();
+        droite();
+        bas();
+        bas();
+        droite();
+    }
+    
+}
