0f6f90be3c14fff19bfb556522bfb8dd1be5741b
Thomas <akgx99@gmail.com>
2019-07-25 10:15:29 +0200

    add level 1 / hello world

---
+++ level 1/Affichage de texte, suite d'instructions/Hello wold!/Problem.java
@@ -0,0 +1,16 @@
+package FranceIOI;
+
+/**
+ *
+ * @author thomas
+ */
+public class Problem {
+
+    /**
+     * Écrivez un programme permettant au robot d'afficher précisément le texte Hello world!.
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello world!");
+    }
+    
+}
