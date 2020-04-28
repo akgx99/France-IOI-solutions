d80e028825a662825ae9288a368b4917f7a551cf
Thomas Bénas <benas.thomas@gmail.com>
2020-04-28 14:27:29 +0200

    Level 1.8 completed

--- level 1/Conditions avancées, opérateurs booléens/Zones de couleurs/Main.java
+++ level 1/Conditions avancées, opérateurs booléens/Zones de couleurs/Main.java
@@ -7,6 +7,23 @@ class Main
 {
    public static void main(String[] args)
    {
-     
+      Scanner input = new Scanner(System.in);
+      int nbJetons = input.nextInt();
+
+      for(int i = 0; i < nbJetons ; i++)
+      {
+         int x = input.nextInt();
+         int y = input.nextInt();
+
+         if (x < 0 || x > 90 || y < 0 || y > 70) {
+            System.out.println("En dehors de la feuille");
+         } else if (y > 60 && ((x > 15 && x < 45) || (x > 60 && x < 85))) {
+            System.out.println("Dans une zone rouge");
+         } else if (x > 10 && x < 85 && y > 10 && y < 55 && !(x > 25 && x < 50 && y > 20 && y < 45)) {
+            System.out.println("Dans une zone bleue");
+         } else {
+            System.out.println("Dans une zone jaune");
+         }
+      }
    }
 }
